031966bfc276edf431a39abcdd8b866ea4c80e84
kokosanki <os_mo@wp.pl>
2020-07-01 22:30:53 +0200

    padding ol fixes

--- src/views/Completed.vue
+++ src/views/Completed.vue
@@ -16,10 +16,14 @@ export default {
 <style lang="scss" scoped>
 .todo {
   margin-top: 5px;
-  width: 80vw;
   box-sizing: border-box;
   border-top: 2px solid #108d8f;
   border-bottom: 2px solid #108d8f;
   padding: 5px 2.5px;
+}
+@media (max-width: 800px) {
+  ol {
+    padding: 0;
+  }
 }
 </style>
